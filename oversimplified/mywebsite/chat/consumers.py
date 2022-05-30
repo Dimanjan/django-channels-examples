6889dc237af7051417ba41529e7555b24b7422ff
Dimanjan <dimanjandahal@gmail.com>
2022-05-30 16:02:27 +0545

    channels

---
+++ oversimplified/mywebsite/chat/consumers.py
@@ -0,0 +1,22 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+        self.send(text_data=json.dumps({
+            'message': 'connected'
+        }))
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        print(message)
+        self.send(text_data=json.dumps({
+            'message': message,
+            'type':'chat'
+        }))
